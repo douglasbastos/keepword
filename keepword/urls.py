e3f50fa5601cdaa5004e5d600db78bce442663fd
Douglas Bastos <douglashsb@gmail.com>
2016-05-26 02:10:59 -0300

    Adding urls da api

--- keepword/urls.py
+++ keepword/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from rest_framework import routers
+from django.conf.urls import url, include
 from django.contrib import admin
+from core import views
+
+router = routers.DefaultRouter()
+router.register(r'feedbacks', views.FeedbackViewSet)
+router.register(r'words', views.WordViewSet)
 
 urlpatterns = [
+    url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^admin/', admin.site.urls),
 ]
