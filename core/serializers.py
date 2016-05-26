6ca0bc499fb61d457f1184b845d6442f425b40a3
douglasbastos <douglashsb@gmail.com>
2016-05-26 01:48:51 -0300

    Creatins serializeres

---
+++ core/serializers.py
@@ -0,0 +1,15 @@
+# coding: utf-8
+from core.models import Feedback, Word
+from rest_framework import serializers
+
+
+class WordSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Word
+        fields = ('word', 'level', 'score', 'answer')
+
+
+class FeedbackSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Feedback
+        fields = ('gostei', 'evaluation', 'frase_id')
